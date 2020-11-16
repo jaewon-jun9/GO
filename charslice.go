58e79ca919ca5ce990481e437358bd1a0851ae21
Jung Jaewon <58683003+jaewon-jun9@users.noreply.github.com>
2020-11-16 14:10:52 +0900

    Create charslice.go

---
+++ charslice.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+    var s1 string="GO GO GO"
+    var s2 string="KOREA"
+    var s3 string="MOONJAEIN"
+    fmt.Println(len(s1))
+    fmt.Println(len(s2))
+    fmt.Println(len(s3))
+	fmt.Println(s1)
+    fmt.Println(s2)
+    fmt.Println(s3)
+    for i := 0; i < len(s1); i++ {
+        fmt.Println(s1[i:i+1])
+    }
+    for i := 0; i < len(s2); i++ {
+        fmt.Println(s2[i:i+1])
+    }
+    for i := 0; i < len(s3); i++ {
+        fmt.Println(s3[i:i+1])
+    }
+}
